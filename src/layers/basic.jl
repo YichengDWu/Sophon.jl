7b75e0261b39476234d839a2b5a05692c116f7b4
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-08-06 23:47:07 -0600

    format

--- src/layers/basic.jl
+++ src/layers/basic.jl
@@ -1,35 +1,37 @@
 struct FourierFeature <: AbstractExplicitLayer
-    in_dims
-    out_dims
-    num_modes
-    std
+    in_dims::Any
+    out_dims::Any
+    num_modes::Any
+    std::Any
 end
 
-FourierFeature(in_dims::Int; num_modes::Int, std::Number = 10f0) = FourierFeature(in_dims, num_modes * 2, num_modes, std)
-function FourierFeature(int_dims::Int, out_dims::Int; std::Number = 10f0)
+function FourierFeature(in_dims::Int; num_modes::Int, std::Number=10.0f0)
+    return FourierFeature(in_dims, num_modes * 2, num_modes, std)
+end
+function FourierFeature(int_dims::Int, out_dims::Int; std::Number=10.0f0)
     @assert iseven(out_dims) "The output dimension must be even"
-    FourierFeature(int_dims, out_dims, out_dims ÷ 2, std)
+    return FourierFeature(int_dims, out_dims, out_dims ÷ 2, std)
 end
 
-function FourierFeature(ch::Pair{Int, Int}; std::Number = 10f0)
-    FourierFeature(first(ch), last(ch); std)
+function FourierFeature(ch::Pair{Int, Int}; std::Number=10.0f0)
+    return FourierFeature(first(ch), last(ch); std)
 end
 
 function initialstates(rng::AbstractRNG, f::FourierFeature)
     modes = randn(rng, Float32, f.num_modes, f.in_dims) .* f.std
-    return (modes = modes,)
+    return (modes=modes,)
 end
 
 function (f::FourierFeature)(x::AbstractVecOrMat, ps, st::NamedTuple)
     x = st.modes * x
     x = 2 * eltype(x)(π) .* x
-    return cat(sin.(x), cos.(x); dims = 1), st
+    return cat(sin.(x), cos.(x); dims=1), st
 end
 
 function (f::FourierFeature)(x::AbstractArray, ps, st::NamedTuple)
     x = batched_mul(st.modes, x)
     x = 2 * eltype(x)(π) .* x
-    return cat(sin.(x), cos.(x); dims = 1), st
+    return cat(sin.(x), cos.(x); dims=1), st
 end
 
 function Base.show(io::IO, f::FourierFeature)
