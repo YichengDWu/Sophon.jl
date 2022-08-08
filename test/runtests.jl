d4907a88e542af472aa77cccd15e5bd330dc1b88
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-08-08 13:08:28 -0600

    test

--- test/runtests.jl
+++ test/runtests.jl
@@ -19,24 +19,24 @@ rng = Random.default_rng()
         y, st = f4(rand(Float32, 2, 2), ps, st)
         @test size(y) == (4, 2)
     end
-    @testset "PINNAttentionNet" begin
+    @testset "PINNAttention" begin
         x = rand(Float32, 3, 5)
         layers = Chain(Dense(4, 4, relu), Dense(4, 4, relu), Dense(4, 4, relu))
-        m = PINNAttentionNet(Dense(3, 4, relu), Dense(3, 4, relu), Dense(3, 4, relu);
+        m = PINNAttention(Dense(3, 4, relu), Dense(3, 4, relu), Dense(3, 4, relu);
                              fusion_layers=layers)
         ps, st = Lux.setup(rng, m)
         y, st = m(x, ps, st)
         @test size(y) == (4, 5)
 
-        m2 = PINNAttentionNet(3, 4, relu; fusion_layers=layers)
+        m2 = PINNAttention(3, 4, relu; fusion_layers=layers)
         ps2, st2 = Lux.setup(rng, m2)
         y2, st2 = m2(x, ps2, st2)
         @test size(y2) == (4, 5)
     end
-    @testset "MultiscaleFourierNet" begin
+    @testset "MultiscaleFourier" begin
         x = rand(Float32, 2, 5)
         chain = Chain(Dense(4, 4, relu), Dense(4, 4, relu), Dense(4, 4, relu))
-        m = MultiscaleFourierNet(2,1,4,chain; std = [1,20,50])
+        m = MultiscaleFourier(2,1,4,chain; std = [1,20,50])
         ps, st = Lux.setup(rng, m)
         y, st = m(x, ps, st)
         @test size(y) == (1, 5)
