f8ce293855be2861de5d61b017363439e603e134
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-08-09 19:35:44 -0600

    docs

--- src/layers/nets.jl
+++ src/layers/nets.jl
@@ -124,6 +124,9 @@ end
 """
     Siren(in_dims::Int, hidden_dim::Int, num_layers::Int; omega = 30f0)
     Siren(in_dims::Int, hidden_dims::NTuple{N, T}; omega = 30f0) where {N, T <: Int}
+
+## K
+  -`omega`: The `ω₀` used for the first layer.
 """
 function Siren(in_dims::Int, hidden_dim::Int, num_layers::Int; omega=30.0f0)
     return Siren(in_dims, ntuple(i -> hidden_dim, num_layers); omega=omega)
