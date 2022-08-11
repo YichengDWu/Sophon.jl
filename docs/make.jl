da84ffe3d056a7c9391889e33dd6d31491f134aa
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-08-11 16:42:09 -0600

    doctest

--- docs/make.jl
+++ docs/make.jl
@@ -10,9 +10,19 @@ makedocs(; modules=[Sophon],
          format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
                                 canonical="https://MilkshakeForReal.github.io/Sophon.jl",
                                 edit_link="main", assets=String[]),
-         pages=["Home" => "index.md",
-                "Tutorials" => [
-                    "Fitting a nonlinear discontinuous function" => "tutorials/discontinuous.md",
-                ]])
+         strict=[
+             :doctest,
+             :linkcheck,
+             :parse_error,
+             :example_block,
+             # Other available options are
+             # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
+         ],
+         pages=[
+             "Home" => "index.md",
+             "Tutorials" => [
+                 "Fitting a nonlinear discontinuous function" => "tutorials/discontinuous.md",
+             ],
+         ])
 
 deploydocs(; repo="github.com/MilkshakeForReal/Sophon.jl", devbranch="main")
