3bd21e3c5e050d5f47e5bb45c9882e862091b880
Shambles <yicheng.wu@ucalgary.ca>
2022-08-19 11:12:12 -0600

    Update activations.jl

--- src/activations.jl
+++ src/activations.jl
@@ -10,7 +10,7 @@ e^{\frac{-0.5 x^{2}}{a^{2}}}
 [1] Ramasinghe, Sameera, and Simon Lucey. "Beyond periodicity: Towards a unifying framework for activations in coordinate-mlps." arXiv preprint arXiv:2111.15135 (2021).
 
 """
-gaussian(x, a=0.2) = exp(-(x / a)^2 / 2)
+gaussian(x, a=0.2) = exp(-(x / NNlib.oftf(x, a))^2 / 2)
 quadratic(x, a=1) = 1 / (1 + (NNlib.oftf(x, a) * x)^2)
 laplacian(x, a=0.01) = exp(-abs(x) / NNlib.oftf(x, a))
 expsin(x, a=1) = exp(-sin(a * x))
