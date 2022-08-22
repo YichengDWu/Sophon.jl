f72efc130ffd5e7ea848800783c1b9e3263a1fd2
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-08-21 19:03:43 -0600

    put it in example

--- src/activations.jl
+++ src/activations.jl
@@ -16,12 +16,14 @@ multiquadratic(x, a=10) = 1 / sqrt((1 + (NNlib.oftf(x, a) * x)^2))
 laplacian(x, a=0.01) = exp(-abs(x) / NNlib.oftf(x, a))
 expsin(x, a=1) = xp(-sin(a * x))
 
-"""
+@doc raw"""
     wu(x,a=1)
+
+An activation function I designed for use in coordinate-mlps.
+
 ```math
 \frac{x\left(5 x^{2}-1\right)}{\left(1+x^{2}\right)^{4}}
 ```
-An activation function I designed for use in coordinate-mlps.
 """
 function wu(x,a=1)
     x = NNlib.oftf(x, a)*x
