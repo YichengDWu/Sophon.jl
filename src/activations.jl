10497834aa319ecb0ea19c3dbb7c9c27ec6d510d
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-08-21 18:48:21 -0600

    Update activations.jl

--- src/activations.jl
+++ src/activations.jl
@@ -14,8 +14,16 @@ gaussian(x, a=0.2) = exp(-(x / NNlib.oftf(x, a))^2 / 2)
 quadratic(x, a=5) = 1 / (1 + (NNlib.oftf(x, a) * x)^2)
 multiquadratic(x, a=10) = 1 / sqrt((1 + (NNlib.oftf(x, a) * x)^2))
 laplacian(x, a=0.01) = exp(-abs(x) / NNlib.oftf(x, a))
-expsin(x, a=1) = exp(-sin(a * x))
-function wu(x,a=1) =
+expsin(x, a=1) = xp(-sin(a * x))
+
+"""
+    wu(x,a=1)
+```math
+\frac{x\left(5 x^{2}-1\right)}{\left(1+x^{2}\right)^{4}}
+```
+An activation function I designed for use in coordinate-mlps.
+"""
+function wu(x,a=1)
     x = NNlib.oftf(x, a)*x
     return x*(5*x^2-1)/(1+x^2)^4
 end
