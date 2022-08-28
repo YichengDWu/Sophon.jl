b9bba6698136d28c3dfb95b2fd7e15c4cf464545
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-08-27 21:26:50 -0600

    Update nets.jl

--- src/layers/nets.jl
+++ src/layers/nets.jl
@@ -147,7 +147,7 @@ function SirenAttention(in_dims::Int, out_dims::Int, activation::Function=relu;
 end
 
 """
-    MultiscaleFourier(in_dims::Int, layer_sizes::NTuple, activation, modes::NTuple)
+    MultiscaleFourier(ayer_sizes::NTuple, activation, modes::NTuple)
 
 Multi-scale Fourier Feature Net.
 
@@ -165,7 +165,7 @@ x → FourierFeature → FullyConnected → y
 # Examples
 
 ```julia
-julia> m = MultiscaleFourier(2, (30, 30, 1), sin; modes=(1 => 10, 10 => 10, 50 => 10))
+julia> MultiscaleFourier((2, 30, 30, 1), sin, (1 => 10, 10 => 10, 50 => 10))
 Chain(
     layer_1 = FourierFeature(2 => 60),
     layer_2 = Dense(60 => 30, sin),     # 1_830 parameters
@@ -173,6 +173,14 @@ Chain(
     layer_4 = Dense(30 => 1),           # 31 parameters
 )         # Total: 2_791 parameters,
           #        plus 60 states, summarysize 112 bytes.
+julia> MultiscaleFourier((2, 30, 30, 1), sin, (1,2,3,4))
+Chain(
+    layer_1 = FourierFeature(2 => 16),
+    layer_2 = Dense(16 => 30, sin),     # 510 parameters
+    layer_3 = Dense(30 => 30, sin),     # 930 parameters
+    layer_4 = Dense(30 => 1),           # 31 parameters
+)         # Total: 1_471 parameters,
+          #        plus 4 states, summarysize 96 bytes.
 ```
 
 # Reference
