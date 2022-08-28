265176e3856b1cf1955197e8e5e57fa757068479
Shambles <yicheng.wu@ucalgary.ca>
2022-08-28 09:40:54 -0600

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -10,7 +10,6 @@ bib = CitationBibliography(joinpath(@__DIR__, "bibliography.bib"); sorting=:nyt)
 DocMeta.setdocmeta!(Sophon, :DocTestSetup, :(using Sophon); recursive=true)
 
 makedocs(bib; modules=[Sophon],
-         authors="MilkshakeForReal <yicheng.wu@ucalgary.ca> and contributors",
          repo="https://github.com/MilkshakeForReal/Sophon.jl/blob/{commit}{path}#{line}",
          sitename="Sophon.jl",
          format=Documenter.HTML(; prettyurls=get(ENV, "CI", "false") == "true",
