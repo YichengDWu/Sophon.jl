84b3099a8e8735d119fcde0f8f2febbcfcceea25
Shambles <yicheng.wu@ucalgary.ca>
2022-08-29 22:21:28 -0600

    Update basic.jl

--- src/layers/basic.jl
+++ src/layers/basic.jl
@@ -11,15 +11,15 @@ Fourier Feature Network.
         Case1: A tuple of pairs of `std => out_dims`, where `std` is the standard deviation of
         the Gaussian distribution, and `out_dims` is the corresponding number of output dimensions.
 
-            ```math
-            \phi^{(i)}(x)=\left[\sin \left(2 \pi W^{(i)} x\right) ; \cos 2 \pi W^{(i)} x\right], \quad W^{(i)} \sim \mathcal{N}\left(0, \sigma^{(i)}\right)
-            ```
+```math
+\phi^{(i)}(x)=\left[\sin \left(2 \pi W^{(i)} x\right) ; \cos 2 \pi W^{(i)} x\right], \quad W^{(i)} \sim \mathcal{N}\left(0, \sigma^{(i)}\right)
+```
 
         Case2: A tuple of frequencies `(f1,f2,...,fn)`.
 
-            ```math
-            \phi^{(i)}(x)=\left[\sin \left(2 \pi f_i x\right) ; \cos 2 \pi f_i x\right]
-            ```
+```math
+\phi^{(i)}(x)=\left[\sin \left(2 \pi f_i x\right) ; \cos 2 \pi f_i x\right]
+```
 
 # Inputs
 
