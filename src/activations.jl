9f6c8f8b2fe49a6624426995a6e17029dc3c0cbd
Shambles <yicheng.wu@ucalgary.ca>
2022-09-06 00:25:42 -0600

    Update activations.jl

--- src/activations.jl
+++ src/activations.jl
@@ -10,7 +10,7 @@ e^{\frac{- x^{2}}{2a^{2}}}
 [ramasinghe2021beyond](@cite)
 """
 gaussian(x, a=0.2) = exp(-(x / NNlib.oftf(x, a))^2 / 2)
-quadratic(x, a=5) = 1 / (1 + (NNlib.oftf(x, a) * x)^2)
+quadratic(x, a=1) = 1 / (1 + (NNlib.oftf(x, a) * x)^2)
 multiquadratic(x, a=10) = 1 / sqrt((1 + (NNlib.oftf(x, a) * x)^2))
 laplacian(x, a=0.01) = exp(-abs(x) / NNlib.oftf(x, a))
 expsin(x, a=1) = xp(-sin(a * x))
