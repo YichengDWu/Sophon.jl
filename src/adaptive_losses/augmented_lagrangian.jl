abd0f0a2253a03f4dbb4f3dec5c494f385780882
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-09-11 17:04:38 -0600

    correct

--- src/adaptive_losses/augmented_lagrangian.jl
+++ src/adaptive_losses/augmented_lagrangian.jl
@@ -2,15 +2,14 @@ mutable struct AugmentedLagrangian{T} <: NeuralPDE.AbstractAdaptiveLoss
     bc_loss_weights::Vector{T}
     additional_loss_weights::Vector{T}
     ϵ::T
-    η::T
     μ_max::T
+    η::T
     μ::Float64
 end
 
 function AugmentedLagrangian(pde_loss_lenghth::Int, bc_loss_length::Int,
     additional_loss_length::Int=0;
     ϵ::Float64 = 1e-8,
-    η::Float64 = 1e-2,
     μ_max::Float64=1e4)
 
     AugmentedLagrangian{Float64}(zeros(Float64, bc_loss_length),
@@ -18,6 +17,7 @@ function AugmentedLagrangian(pde_loss_lenghth::Int, bc_loss_length::Int,
                                  ϵ,
                                  η,
                                  μ_max,
+                                 0.0,
                                  1.0)
 end
 
@@ -31,16 +31,16 @@ function generate_full_loss_function(pinnrep::NeuralPDE.PINNRepresentation,
         bc_losses = [bc_loss_function(θ) for bc_loss_function in bc_loss_functions]
 
         bc_loss = sum(adaloss.bc_loss_weights .* bc_losses)
-        penalty = sum(abs, bc_losses)
+        penalty = sum(abs2, bc_losses)
 
-        loss = pde_loss + bc_loss + adaloss.μ/2 * penalty^2
+        loss = pde_loss + bc_loss + adaloss.μ/2 * penalty
 
         ChainRulesCore.@ignore_derivatives begin
-            if penalty ≥ 0.25*adaloss.η && penalty > adaloss.ϵ
+            if √penalty ≥ 0.25*adaloss.η && √penalty > adaloss.ϵ
                 adaloss.μ = min(adaloss.μ*2.0, adaloss.μ_max)
-                adaloss.bc_loss_weights = adaloss.bc_loss_weights .+ adaloss.μ .* bc_losses
-                adaloss.η  = penalty
+                adaloss.bc_loss_weights .= adaloss.bc_loss_weights .+ adaloss.μ .* bc_losses
             end
+            adaloss.η  = √penalty
         end
         return loss
     end
