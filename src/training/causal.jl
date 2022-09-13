c466af29b494fab27115c1a2ea9441b8624e2ee7
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-09-13 01:28:42 -0600

    Update causal.jl

--- src/training/causal.jl
+++ src/training/causal.jl
@@ -10,21 +10,23 @@
 [1] Wang S, Sankaran S, Perdikaris P. Respecting causality is all you need for training physics-informed neural networks[J]. arXiv preprint arXiv:2203.07404, 2022.
 
 !!!note
-    Don't directly use it with `BFGS` or `LBFGS`. It will not work. Rewrite a callback function
+    Don't directly use it with `BFGS` or `LBFGS`. It will be slow. Rewrite a callback function
     to set `CausalTraining.reweight = true`.
 """
-struct CausalTraining <: NeuralPDE.AbstractTrainingStrategy
+mutable struct CausalTraining <: NeuralPDE.AbstractTrainingStrategy
     points::Int64
     init_points::Int64
     bc_points::Int64
     epsilon::AbstractSchedule
     sampling_alg::QuasiMonteCarlo.SamplingAlgorithm
+    reweight::Bool
+    W::Maitrix{Float64}
 end
 
 function CausalTraining(points; epsilon, init_points=points, bc_points=points,
                         sampling_alg=LatinHypercubeSample())
     epsilon = epsilon isa Real ? Constant(Float64(epsilon)) : epsilon
-    return CausalTraining(points, init_points, bc_points, epsilon, sampling_alg)
+    return CausalTraining(points, init_points, bc_points, epsilon, sampling_alg, true, Array{Float64}(undef,1,points))
 end
 
 function NeuralPDE.merge_strategy_with_loss_function(pinnrep::NeuralPDE.PINNRepresentation,
@@ -60,6 +62,8 @@ function NeuralPDE.merge_strategy_with_loss_function(pinnrep::NeuralPDE.PINNRepr
                            for (_loss, bound) in zip(datafree_init_loss_function,
                                                      init_bounds)]
 
+    strategy.W = ones(Float64, length(datafree_pde_loss_function), strategy.points)
+
     if isempty(real_datafree_bc_loss_function)
         pde_loss_functions = get_pde_loss_function(init_loss_functions,
                                                    datafree_pde_loss_function, pde_bounds,
@@ -88,11 +92,14 @@ function get_pde_and_bc_loss_function(init_loss_functions, datafree_bc_loss_func
 
     bc_sets = [NeuralPDE.generate_quasi_random_points(bc_points, bound, eltypeθ,
                                                       sampling_alg) for bound in bc_bounds]
+    bc_sets = [adapt(device, set) for set in bc_sets]
 
     pde_sets = NeuralPDE.generate_quasi_random_points(points, pde_bounds, eltypeθ,
                                                       sampling_alg)
     pde_sets = [sortslices(set; dims=2, alg=InsertionSort,
                            lt=(x, y) -> isless(x[tidx], y[tidx])) for set in pde_sets]
+
+    pde_set = [adapt(device, set) for set in pde_sets]
 
     function get_bc_loss_func(bc_loss_func, bc_set)
         return θ -> begin
@@ -107,14 +114,12 @@ function get_pde_and_bc_loss_function(init_loss_functions, datafree_bc_loss_func
 
     function get_pde_loss_function(datafree_pde_loss_func, pde_set)
         return θ -> begin
-            ChainRulesCore.@ignore_derivatives begin
-                L_init = reduce(+, [loss_func(θ) for loss_func in init_loss_functions])
-                set_ = adapt(device, pde_set)
-                L_pde = abs2.(datafree_pde_loss_func(set_, θ))
-                L = hcat(adapt(device, [L_init;;]), L_pde[:, 1:(end - 1)])
-                W = exp.(-ϵ / points .* cumsum(L; dims=2))
-            end
-            mean(abs2, W .* datafree_pde_loss_func(set_, θ))
+            L_init = ChainRulesCore.@ignore_derivativesreduce(+, [loss_func(θ) for loss_func in init_loss_functions])
+            L_pde = ChainRulesCore.@ignore_derivatives abs2.(datafree_pde_loss_func(pde_set, θ))
+            L = ChainRulesCore.@ignore_derivatives hcat(adapt(device, [L_init;;]), L_pde[:, 1:(end - 1)])
+            W = ChainRulesCore.@ignore_derivatives exp.(-ϵ / points .* cumsum(L; dims=2))
+
+            mean(abs2, W .* datafree_pde_loss_func(pde_set, θ))
         end
     end
 
