30df00c171a2213205dfec0c2f8295b209a4c079
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-09-22 17:28:28 -0600

    Update pinnsampler.jl

--- src/compact/NeuralPDE/pinnsampler.jl
+++ src/compact/NeuralPDE/pinnsampler.jl
@@ -15,31 +15,29 @@ function sample(::PDESystem, ::PINNSampler, ::AbstractTrainingAlg) end
 Sampler to generate the datasets for PDE and boundary conditions using a quisa-random sampling algorithm.
 It momerizes the domain of the PDE and the boundary conditions, and you can call `sample` on it to generate the datasets.
 """
-struct QuasiRandomSampler{T, N1, N2, S} <: PINNSampler{T, N1, N2}
-    pde_points::NTuple{N1, Int}
-    bcs_points::NTuple{N2, Int}
+struct QuasiRandomSampler{T, P, B, S} <: PINNSampler{T, N1, N2}
+    pde_points::P
+    bcs_points::B
     sampling_alg::S
 end
 
 function QuasiRandomSampler(pde_points, bcs_points=pde_points;
                             device_type::Type=Array{Float64},
                             sampling_alg=LatinHypercubeSample())
-    N1 = length(pde_bounds)
-    N2 = length(bcs_bounds)
-    pde_points = length(pde_points) == 1 ? ntuple(_ -> first(pde_points), N1) :
-                 Tuple(pde_points)
-    bcs_points = length(bcs_points) == 1 ? ntuple(_ -> first(bcs_points), N2) :
-                 Tuple(bcs_points)
-
-    return QuasiRandomSampler{device_type, N1, N2, typeof(sampling_alg)}(pde_points,
-                                                                         bcs_points,
-                                                                         sampling_alg)
+    return QuasiRandomSampler{device_type, typeof(pde_points), typeof(bcs_points),
+                              typeof(sampling_alg)}(pde_points, bcs_points, sampling_alg)
 end
 
-function sample(pde::PDESystem, sampler::QuasiRandomSampler{device_type}, strategy) where {device_type}
+function sample(pde::PDESystem, sampler::QuasiRandomSampler{device_type},
+                strategy) where {device_type}
     eltypeθ = eltype(device_type)
     (; pde_points, bcs_points, sampling_alg) = sampler
     pde_bounds, bcs_bounds = get_bounds(pde)
+
+    pde_points = length(pde_points) == 1 ?
+                 ntuple(_ -> first(pde_points), length(pde_bounds)) : Tuple(pde_points)
+    bcs_points = length(bcs_points) == 1 ?
+                 ntuple(_ -> first(bcs_points), length(bcs_bounds)) : Tuple(bcs_points)
 
     pde_datasets = [NeuralPDE.generate_quasi_random_points(points, bound, eltypeθ,
                                                            sampling_alg)
