651385682a10807b1d5f5b4f5b559cbc0403424c
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-10-01 10:20:14 -0600

    update

---
+++ docs/src/tutorials/sod.jl
@@ -0,0 +1,73 @@
+using Optimization, OptimizationOptimJL, Plots
+using ModelingToolkit, IntervalSets
+using Sophon
+using ChainRulesCore
+
+@parameters t, x
+@variables u(..), ρ(..), p(..)
+Dₓ = Differential(x)
+Dₜ = Differential(t)
+
+u_bound(x) = ifelse(x < 0.5, 0.0, 0.0)
+@register u_bound(x)
+ρ_bound(x) = ifelse(x < 0.5, 1.0, 0.125)
+@register ρ_bound(x)
+p_bound(x) = ifelse(x < 0.5, 1.0, 0.1)
+@register p_bound(x)
+
+bcs = [
+    ρ(0, x) ~ ρ_bound(x),
+    u(0, x) ~ u_bound(x),
+    p(0, x) ~ p_bound(x),
+    u(t, 0) ~ 0.0,
+    u(t, 1) ~ 0.0
+]
+
+γ = 1.4
+E(t,x) = p(t, x) / (γ - 1) + 0.5 * ρ(t, x) * u(t, x) * u(t, x)
+
+eqs = [
+    Dₜ(ρ(t, x)) + Dₓ(ρ(t, x) * u(t, x)) ~ 0.0,
+    Dₜ(ρ(t, x) * u(t, x)) + Dₓ(ρ(t, x) * u(t, x) * u(t, x) + p(t, x)) ~ 0.0,
+    Dₜ(E(t,x)) + Dₓ(u(t, x) * (E(t,x) + p(t, x))) ~ 0.0
+]
+
+t_min, t_max = 0.0, 0.14
+x_min, x_max = 0.0, 1.0
+domains = [t ∈ Interval(t_min, t_max),
+           x ∈ Interval(x_min, x_max)]
+
+@named pde_system = PDESystem(eqs, bcs, domains, [t, x], [u(t, x), ρ(t, x), p(t, x)])
+
+pinn = PINN(u = FullyConnected(2, 1, tanh; num_layers = 3, hidden_dims = 16),
+            ρ = FullyConnected(2, 1, tanh; num_layers = 3, hidden_dims = 16),
+            p = FullyConnected(2, 1, tanh; num_layers = 3, hidden_dims = 16))
+
+sampler = QuasiRandomSampler(1000, 100)
+
+function pde_weights(phi, x, θ)
+    ux = ChainRulesCore.@ignore_derivatives Sophon.numeric_derivative(phi.u, x, θ.u, 1, 1)
+    return inv.(5*(abs.(ux) .- ux) .+ 1)
+end
+
+strategy = AdaptiveTraining(pde_weights, 100)
+
+prob = Sophon.discretize(pde_system, pinn, sampler, strategy)
+
+# Residuals are shown
+callback = function (p, l)
+    println("loss: ", l)
+    return false
+end
+
+res = Optimization.solve(prob, LBFGS(); maxiters=2000, callback = callback)
+
+θ = res.u
+
+phi = pinn.phi
+xs = x_min:0.01:x_max |> collect
+
+phi = pinn.phi
+p1 = plot(xs, [first(phi.u([t_max, x], θ.u)) for x in xs], label ="u(t=1,x)")
+p2 = plot!(xs, [first(phi.ρ([t_max, x], θ.ρ)) for x in xs], label ="ρ(t=1,x)")
+p3 = plot!(xs, [first(phi.p([t_max, x], θ.p)) for x in xs], label ="p(t=1,x)")
--- src/compact/NeuralPDE/training_strategies.jl
+++ src/compact/NeuralPDE/training_strategies.jl
@@ -42,7 +42,7 @@ end
     AdaptiveTraining(pde_weights, bcs_weights)
 
 Adaptive weights for the loss functions. Here `pde_weights` and `bcs_weights` are
-functions that take in `(phi, θ, data)` and return the point-wise weights. Note that `bcs_weights` can be
+functions that take in `(phi, x, θ)` and return the point-wise weights. Note that `bcs_weights` can be
 real numbers but they will be converted to functions that return the same numbers.
 """
 struct AdaptiveTraining{P,B} <: AbstractTrainingAlg
@@ -51,22 +51,22 @@ struct AdaptiveTraining{P,B} <: AbstractTrainingAlg
 end
 
 function AdaptiveTraining(pde_weights::Function, bcs_weights::Real)
-    _bcs_weights = (phi, θ, p) -> bcs_weights
+    _bcs_weights = (phi, cord, θ) -> bcs_weights
     return AdaptiveTraining{typeof(pde_weights), typeof(_bcs_weights)}(pde_weights, _bcs_weights)
 end
 
 function AdaptiveTraining(pde_weights::Function, bcs_weights::NTuple{N, <:Real}) where {N}
-    _bcs_weights = map(w -> (phi, θ, p) -> w, bcs_weights)
+    _bcs_weights = map(w -> (phi, cord, θ) -> w, bcs_weights)
     return AdaptiveTraining{typeof(pde_weights), typeof(_bcs_weights)}(pde_weights, _bcs_weights)
 end
 
 function AdaptiveTraining(pde_weights::Tuple{Vararg{<:Function}}, bcs_weights::Int)
-    _bcs_weights = (phi, θ, p) -> bcs_weights
+    _bcs_weights = (phi, cord, θ) -> bcs_weights
     return AdaptiveTraining{typeof(pde_weights), typeof(_bcs_weights)}(pde_weights, _bcs_weights)
 end
 
 function AdaptiveTraining(pde_weights::Tuple{Vararg{<:Function}}, bcs_weights::NTuple{N, <:Real}) where {N}
-    _bcs_weights = map(w -> (phi, θ, p) -> w, bcs_weights)
+    _bcs_weights = map(w -> (phi, cord, θ) -> w, bcs_weights)
     return AdaptiveTraining{typeof(pde_weights), typeof(_bcs_weights)}(pde_weights, _bcs_weights)
 end
 
@@ -77,8 +77,8 @@ function scalarize(strategy::AdaptiveTraining, phi, datafree_pde_loss_function,
     N1 = length(datafree_pde_loss_function)
     N2 = length(datafree_bc_loss_function)
 
-    pde_weights = length(pde_weights) == 1 ? ntuple(_ -> first(pde_weights), N1) : pde_weights
-    bcs_weights = length(bcs_weights) == 1 ? ntuple(_ -> first(bcs_weights), N2) : bcs_weights
+    pde_weights = pde_weights isa Function ? ntuple(_ -> pde_weights, N1) : pde_weights
+    bcs_weights = bcs_weights isa Function ? ntuple(_ -> bcs_weights, N2) : bcs_weights
 
     f = scalarize(phi, (pde_weights..., bcs_weights...),
                   (datafree_pde_loss_function..., datafree_bc_loss_function...))
@@ -88,9 +88,9 @@ end
 
 function scalarize(phi, weights::Tuple{Vararg{<:Function}}, datafree_loss_function::Tuple)
     N = length(datafree_loss_function)
-    ex = :(mean($(weights[1])(phi, θ, p[1]) .* abs2.($(datafree_loss_function[1])(p[1], θ))))
+    ex = :(mean($(weights[1])($phi, p[1], θ) .* abs2.($(datafree_loss_function[1])(p[1], θ))))
     for i in 2:N
-        ex = :(mean($(weights[i])(phi, θ, p[$i]) .* abs2.($(datafree_loss_function[i])(p[$i], θ))) + $ex)
+        ex = :(mean($(weights[i])($phi, p[$i], θ) .* abs2.($(datafree_loss_function[i])(p[$i], θ))) + $ex)
     end
     loss_f = :((θ, p) -> $ex)
     return NeuralPDE.@RuntimeGeneratedFunction(loss_f)
