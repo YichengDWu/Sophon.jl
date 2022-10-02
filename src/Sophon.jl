f699745b47907eb9707bbbe0167b33d413451c42
Yicheng Wu <yicheng.wu@ucalgary.ca>
2022-10-02 01:57:57 -0600

    Update Sophon.jl

--- src/Sophon.jl
+++ src/Sophon.jl
@@ -40,7 +40,7 @@ include("layers/operators.jl")
 export Scheduler, QuasiRandom
 export gaussian, quadratic, laplacian, expsin, multiquadratic
 export FourierFeature, TriplewiseFusion, FullyConnected, Sine, RBF, DiscreteFourierFeature,
-       Scalar, ScalarLayer, SplitFunction
+       ConstantFunction, ScalarLayer, SplitFunction
 export PINNAttention, FourierNet, FourierAttention, Siren, FourierFilterNet, BACON
 export DeepONet
 export PINN, symbolic_discretize, discretize, QuasiRandomSampler, NonAdaptiveTraining,
