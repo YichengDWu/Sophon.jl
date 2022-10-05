441c852ce13a6639eaf7cac60dd4a538a5db723a
Documenter.jl <documenter@juliadocs.github.io>
2022-10-05 01:14:54 +0000

    build based on c0004fb

--- dev/tutorials/sod.jl
+++ dev/tutorials/sod.jl
@@ -31,7 +31,7 @@ domains = [t ∈ Interval(t_min, t_max), x ∈ Interval(x_min, x_max)]
 
 @named pde_system = PDESystem(eqs, bcs, domains, [t, x], [u(t, x), ρ(t, x), p(t, x)])
 
-pinn = PINN(; u=FullyConnected(2, 1, tanh; num_layers=4, hidden_dims=16),
+pinn = PINN(u=FullyConnected(2, 1, tanh; num_layers=4, hidden_dims=16),
             ρ=FullyConnected(2, 1, tanh; num_layers=4, hidden_dims=16),
             p=FullyConnected(2, 1, tanh; num_layers=4, hidden_dims=16))
 
@@ -46,18 +46,17 @@ strategy = AdaptiveTraining(pde_weights, 10)
 
 prob = Sophon.discretize(pde_system, pinn, sampler, strategy)
 
-# Residuals are shown
-callback = function (p, l)
+function callback(p, l)
     println("loss: ", l)
     return false
 end
 
-res = Optimization.solve(prob, LBFGS(); maxiters=2000, callback=callback)
+res = Optimization.solve(prob, BFGS(); maxiters=2000, callback=callback)
 
 for _ in 1:10
     data = Sophon.sample(pde_system, sampler, strategy)
     prob = remake(prob; u0=res.u, p=data)
-    res = Optimization.solve(prob, LBFGS(); maxiters=2000, callback=callback)
+    res = Optimization.solve(prob, BFGS(); maxiters=2000, callback=callback)
 end
 
 θ = res.u
