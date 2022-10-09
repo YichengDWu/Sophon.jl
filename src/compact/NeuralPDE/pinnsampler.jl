c7f4db73fcf41a2649dd4b61064676eea6d99d11
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-10-08 18:41:06 -0600

    dep

--- src/compact/NeuralPDE/pinnsampler.jl
+++ src/compact/NeuralPDE/pinnsampler.jl
@@ -93,18 +93,19 @@ function sample(d::Interval, points::Int, ::SobolSample)
     return sobolsample(points, bounds[1], bounds[2])
 end
 
-function sample(d::SetdiffDomain{S, <:Tuple{<:Rectangle, F}}, points::Int, alg::SobolSample) where {S, F}
+function sample(d::SetdiffDomain{S, <:Tuple{<:Rectangle, F}}, points::Int, alg::QuasiMonteCarlo.SamplingAlgorithm) where {S, F}
     rec = d.domains[1]
-    bounds = get_bounds(rec)
     data = sample(rec, points, alg)
-end 
+    idx = [x ∈ d for d in eachcol(data)]
+    return data[:, idx]
+end
 
-function sample(d::Rectangle, points::Int, sampling_alg:: QuasiMonteCarlo.SamplingAlgorithm)
+function sample(d::Rectangle, points::Int, sampling_alg::QuasiMonteCarlo.SamplingAlgorithm)
     bounds = get_bounds(d)
     return QuasiMonteCarlo.sample(points, bounds[1], bounds[2], sampling_alg)
 end
 
-function sample(d::Interval, points::Int, sampling_alg:: QuasiMonteCarlo.SamplingAlgorithm)
+function sample(d::Interval, points::Int, sampling_alg::QuasiMonteCarlo.SamplingAlgorithm)
     bounds = get_bounds(d)
     return QuasiMonteCarlo.sample(points, bounds[1], bounds[2], sampling_alg)
 end
