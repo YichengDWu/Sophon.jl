41be19431eb327b26f984d3f43a3a3932e25210b
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-10-13 23:57:50 -0600

    Update training_strategies.jl

--- src/compact/NeuralPDE/training_strategies.jl
+++ src/compact/NeuralPDE/training_strategies.jl
@@ -4,6 +4,11 @@ abstract type AbstractTrainingAlg end
     NonAdaptiveTraining(pde_weights=1, bcs_weights=pde_weights)
 
 Fixed weights for the loss functions.
+
+## Arguments
+
+    - `pde_weights`: weights for the PDE loss functions. If a single number is given, it is used for all PDE loss functions.
+    - `bcs_weights`: weights for the boundary conditions loss functions. If a single number is given, it is used for all boundary conditions loss functions.
 """
 struct NonAdaptiveTraining{P, B} <: AbstractTrainingAlg
     pde_weights::P
