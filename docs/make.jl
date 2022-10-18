edc9c789daf985e0956180614103d39975257e21
Yicheng Wu <yicheng.wu@ucalgary.ca>
2022-10-18 16:52:03 -0600

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -37,4 +37,4 @@ makedocs(bib; modules=[Sophon], sitename="Sophon.jl",
              "References" => "references.md",
          ])
 
-deploydocs(; repo="github.com/YichengDWu/Sophon.jl", devbranch="main")
+deploydocs(; repo="github.com/YichengDWu/Sophon.jl", devbranch="main", preview=true)
