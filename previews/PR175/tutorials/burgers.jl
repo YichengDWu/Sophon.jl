41ba3e36917eb0b7d855549ec2eeefca47b1026b
Documenter.jl <documenter@juliadocs.github.io>
2022-10-19 18:55:10 +0000

    build based on dc08d1c

--- previews/PR175/tutorials/burgers.jl
+++ previews/PR175/tutorials/burgers.jl
@@ -24,13 +24,14 @@ Burgers = Sophon.ParametricPDESystem([eq], bcs, [t, x], [u(x,t)], [a(x)])
 chain = DeepONet((50, 50, 50, 50), tanh, (2, 50, 50, 50, 50), tanh)
 pinn = PINN(chain)
 sampler = QuasiRandomSampler(500, 50)
-strategy = NonAdaptiveTraining(1, (1, 20))
+strategy = NonAdaptiveTraining(1, 1)
 
 struct MyFuncSampler <: Sophon.FunctionSampler
     num_fcs::Int
 end
 
-Sophon.sample(::MyFuncSampler) = [cospi, sinpi, x -> cospi(2x), x-> sinpi(2x)]
+Sophon.sample(::MyFuncSampler) = [cospi, sinpi, x -> cospi(2x), x-> sinpi(2x), x -> 0.5*cospi(2x), x -> 0.5*sinpi(2x),
+                                  x -> 0.25*cospi(x), x -> 0.25*sinpi(x), x -> 0.75*cospi(3x), x -> 0.75*sinpi(3x)]
 
 cord_branch_net = [range(0.0, 1.0, length=50)...]
 
@@ -41,15 +42,16 @@ callback = function (p,l)
     return false
 end
 
-@time res = Optimization.solve(prob, BFGS(); maxiters=500, callback=callback)
+@time res = Optimization.solve(prob, BFGS(); maxiters=1000, callback=callback)
 
 using CairoMakie
 
 phi = pinn.phi
-xs = 0.0:0.001:1.0
-ts = 0.0:0.001:1.0
+xs = 0.0:0.01:1.0
+ts = 0.0:0.01:1.0
 
-u0 = reshape(cospi.(cord_branch_net), :, 1)
+f_test(x) = sinpi(x)
+u0 = reshape(f_test.(cord_branch_net), :, 1)
 axis = (xlabel="t", ylabel="x", title="Prediction")
 u_pred = [sum(pinn.phi((u0, [x, t]), res.u)) for x in xs, t in ts]
 fig, ax, hm = heatmap(ts, xs, u_pred', axis=axis, colormap=:jet)
