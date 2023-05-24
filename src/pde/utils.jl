499abd048dcde8f07666c5e8c07161ecc6a016e6
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-05-24 03:29:58 -0600

    Update utils.jl

--- src/pde/utils.jl
+++ src/pde/utils.jl
@@ -16,15 +16,6 @@ This function is only used for the first order derivative.
 """
 forwarddiff(phi, t, εs, order, θ) = ForwardDiff.gradient(sum ∘ Base.Fix2(phi, θ), t)
 
-function finitediff(phi, x, θ, εs, epsilon, order::Val{N}, mixed::Val{true}) where {N}
-    ε = εs[N]
-    _epsilon = ChainRulesCore.@ignore_derivatives epsilon[N]
-    ε = ChainRulesCore.@ignore_derivatives adapt(parameterless_type(x), ε)
-
-    return (finitediff(phi, x .+ ε, θ, @view(εs[1:(end - 1)]), @view(epsilon[1:(end - 1)]), Val(N-1), mixed) .-
-            finitediff(phi, x .- ε, θ, @view(εs[1:(end - 1)]), @view(epsilon[1:(end - 1)]), Val(N-1), mixed)) .* _epsilon ./ 2
-end
-
 @inline function finitediff(phi, x, θ, ε::AbstractVector{T}, h::T, ::Val{1}) where {T<:AbstractFloat}
     return (phi(x .+ ε, θ) .- phi(x .- ε, θ)) .* h ./ 2
 end
