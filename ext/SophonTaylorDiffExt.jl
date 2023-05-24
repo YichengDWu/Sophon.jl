e3e1c394ea6a993b639614e742d638cba0dabd69
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-05-24 03:07:02 -0600

    Update SophonTaylorDiffExt.jl

--- ext/SophonTaylorDiffExt.jl
+++ ext/SophonTaylorDiffExt.jl
@@ -3,23 +3,23 @@ module SophonTaylorDiffExt
 if isdefined(Base, :get_extension)
     using TaylorDiff, NNlib, ChainRulesCore
     import NNlib: tanh_fast
-    import TaylorDiff: derivative, make_taylor, raise, extract_derivative
+    import TaylorDiff: derivative, make_taylor, raise, extract_derivative, value
     import ChainRulesCore: rrule, @thunk
 else
     using ..TaylorDiff, ..NNlib, ..ChainRulesCore
     import ..NNlib: tanh_fast
-    import ..TaylorDiff: derivative, make_taylor, raise, extract_derivative
+    import ..TaylorDiff: derivative, make_taylor, raise, extract_derivative, value
     import ..ChainRulesCore: rrule, @thunk
 end
 
 function tanh_fast(t::TaylorScalar{T,2}) where {T}
-    t0, t1 = TaylorDiff.value(t)
+    t0, t1 = value(t)
     return TaylorScalar{T,2}(frule((NoTangent(), t1), tanh_fast, t0))
 end
 function tanh_fast(t::TaylorScalar{T,N}) where {T,N}
     t1 = TaylorScalar{T,N-1}(t)
     df = 1 - tanh_fast(t1)^2
-    return raise(tanh_fast(TaylorDiff.value(t)[1]), df, t)
+    return raise(tanh_fast(value(t)[1]), df, t)
 end
 
 function rrule(::typeof(*), A::AbstractMatrix{S},
@@ -34,15 +34,20 @@ function rrule(::typeof(*), A::AbstractMatrix{S},
 end
 
 function rrule(::typeof(*), A::AbstractMatrix{S},
-                              t::AbstractMatrix{TaylorScalar{T}}) where {S <: Number, T}
-    project_A = ProjectTo(A)
+               t::AbstractMatrix{TaylorScalar{T}}) where {S <: Number, T}
     project_t = ProjectTo(t)
     function gemv_pullback(x̄)
         X̄ = unthunk(x̄)
         X̂ = reinterpret(reshape, T, X̄)
-        t̂ = reinterpret(reshape, T, t)
+        T̂ = reinterpret(reshape, T, t)
 
-        dA = @thunk(project_A(dropdims(sum(batched_transpose(X̂) ⊠ t̂; dims=3); dims=3)))
+        dA = @thunk begin
+                C = zero(A)
+                @inbounds for n in axes(X̂, 3)
+                    C .+= @views transpose(X̂[:, :, n]) * T̂[:, :, n]
+                end
+                C
+            end
         dB = @thunk(project_t(transpose(A)*X̄))
         NoTangent(), dA, dB
     end
@@ -60,11 +65,12 @@ end
     return extract_derivative(f(t), N)
 end
 
+@inline extract_derivative(t::TaylorScalar, ::Val{N}) where {N} = value(t)[N]
 # batched version
 @inline function derivative(f, x::AbstractMatrix{T}, l::AbstractVector{T},
                             vN::Val{N}) where {T <: Number, N}
     t = broadcast((t0, t1) -> TaylorDiff.make_taylor(t0, t1, vN), x, l)
-    return map(Base.Fix2(extract_derivative, N), f(t))
+    return map(Base.Fix2(extract_derivative, vN), f(t))
 end
 
 end
