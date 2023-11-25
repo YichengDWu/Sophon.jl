798e747755552dfdb487ac992f72f5070dcf7321
Yicheng Wu <yicheng.wu@ucalgary.ca>
2023-11-25 18:22:01 +0800

    fix zygote error by using tuple

--- ext/SophonLuxCUDAExt.jl
+++ ext/SophonLuxCUDAExt.jl
@@ -4,9 +4,8 @@ using Lux, LuxCUDA, Sophon, Optimization, Adapt
 
 function (::LuxCUDADevice)(prob::OptimizationProblem)
     u0 = adapt(CuArray, prob.u0)
-    p = [adapt(CuArray, prob.p[i]) for i in 1:length(prob.p)]
-    prob = remake(prob, u0=u0, p=p)
-    return prob
+    p = Tuple(adapt(CuArray, prob.p[i]) for i in 1:length(prob.p))  # have to use tuple here...
+    return Optimization.OptimizationProblem(prob.f, u0, p)
 end
 
 end
